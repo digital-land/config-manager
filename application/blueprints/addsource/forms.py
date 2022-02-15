fa74f74ae3d5a560da49f0ee3bc0a9d44de08606
Colm Britton <colmjude@gmail.com>
2022-02-15 11:14:13 +0000

    Makes dataset input an accessible autocomplete component

--- application/blueprints/addsource/forms.py
+++ application/blueprints/addsource/forms.py
@@ -11,7 +11,7 @@ class SourceForm(FlaskForm):
             URL(message="Please provide a valid URL"),
         ],
     )
-    dataset = StringField(
+    dataset = SelectField(
         "Dataset", validators=[DataRequired(message="Please provide a dataset")]
     )
     organisation = SelectField(
--- application/blueprints/addsource/views.py
+++ application/blueprints/addsource/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, Blueprint, redirect, url_for
 
 from application.blueprints.addsource.forms import SourceForm
-from application.models import Organisation
+from application.models import Organisation, Dataset
 
 addsource = Blueprint("addsource", __name__, url_prefix="/add-a-source")
 
@@ -13,6 +13,9 @@ def index():
     form.organisation.choices = [("", "")] + [
         (o.organisation, o.name) for o in organisations
     ]
+    datasets = Dataset.query.order_by(Dataset.name).all()
+    # To Do: remove the config datasets from list e.g. field-name
+    form.dataset.choices = [("", "")] + [(d.dataset, d.name) for d in datasets]
 
     if form.validate_on_submit():
         return redirect(url_for("addsource.mappings"))
