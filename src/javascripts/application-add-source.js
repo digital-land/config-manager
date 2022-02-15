6d0957ad6514322afd1d5fa9b7556a882442bd8c
Colm Britton <colmjude@gmail.com>
2022-02-15 13:56:45 +0000

    Adds hint text for autocomplete fields

--- src/javascripts/application-add-source.js
+++ src/javascripts/application-add-source.js
@@ -1,11 +1,9 @@
 /* global accessibleAutocomplete */
 
 accessibleAutocomplete.enhanceSelectElement({
-  defaultValue: 'Start typing organisation name',
   selectElement: document.querySelector('#organisation')
 })
 
 accessibleAutocomplete.enhanceSelectElement({
-  defaultValue: 'Start typing dataset name',
   selectElement: document.querySelector('#dataset')
 })
