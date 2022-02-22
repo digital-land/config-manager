82bfab7f1a58c08547a89c33342f8469057bc855
Adam Shimali <adam.shimali@gmail.com>
2022-02-22 15:07:23 +0000

    Added playwright test for add source.

--- tests/functional/test_application.py
+++ tests/functional/test_application.py
@@ -18,3 +18,43 @@ def test_visit_main_pages(server_process, page):
     assert page.url == f"{BASE_URL}/#"
 
     page.click("text=Data manager")
+
+
+def test_add_source(server_process, page):
+
+    page.goto(BASE_URL)
+    page.click("text=Add a source")
+    assert page.url == f"{BASE_URL}/source/add"
+
+    page.click('input[name="endpoint"]')
+    page.fill(
+        'input[name="endpoint"]',
+        "http://www.nnjpu.org.uk/publications/docdetail.asp?docid=1593",
+    )
+    page.select_option('select[name="dataset"]', "brownfield-land")
+    page.select_option('select[name="organisation"]', "local-authority-eng:KET")
+    page.click("text=Save and continue")
+
+    assert page.url == f"{BASE_URL}/source/add/summary"
+    page.click("text=Url")
+
+    to_check = [
+        ("Url", "http://www.nnjpu.org.uk/publications/docdetail.asp?docid=1593"),
+        ("Dataset", "brownfield-land"),
+        ("Organisation", "local-authority-eng:KET"),
+    ]
+
+    for index, items in enumerate(to_check):
+
+        dt_text = page.text_content(
+            f"dl > .govuk-summary-list__row:nth-child({index+1}) > dt"
+        ).strip()
+        dd_text = page.text_content(
+            f"dl > .govuk-summary-list__row:nth-child({index+1}) > dd"
+        ).strip()
+
+        assert dt_text == items[0]
+        assert dd_text == items[1]
+
+    page.click("text=Save source")
+    assert page.url == f"{BASE_URL}/source/add/finish"
