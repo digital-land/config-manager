40239d40f1e5636aa437c1be634eb4dabf9ebd6c
Colm Britton <colmjude@gmail.com>
2022-02-25 15:36:47 +0000

    Displays latest resources

--- application/blueprints/resource/views.py
+++ application/blueprints/resource/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, render_template
+from flask import Blueprint, jsonify, redirect, render_template, url_for
 
 from application.blueprints.resource.forms import SearchForm
 from application.models import Resource
@@ -6,10 +6,26 @@ from application.models import Resource
 resource_bp = Blueprint("resource", __name__, url_prefix="/resource")
 
 
-@resource_bp.route("/")
-def index():
+@resource_bp.route("/", methods=["GET", "POST"])
+def search():
     form = SearchForm()
-    return render_template("resource/search.html", form=form)
+    if form.validate_on_submit():
+        resource_hash = form.resource.data.strip()
+        resource = Resource.query.get(resource_hash)
+        if resource:
+            return redirect(
+                url_for("resource.resource", resource_hash=resource.resource)
+            )
+        form.resource.errors.append("We don't recognise that hash, try another")
+
+    resources = (
+        Resource.query.filter(Resource.start_date != None)  # noqa: E711
+        .order_by(Resource.start_date.desc())
+        .limit(5)
+        .all()
+    )
+
+    return render_template("resource/search.html", form=form, resources=resources)
 
 
 @resource_bp.route("/<resource_hash>")
