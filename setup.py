03b0afaf96efb1a411bc7122b291df6c1f8e51a9
Toby Petty <tobypetty1@gmail.com>
2022-07-14 11:40:11 +0100

    Fix URL and author in setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from version import get_version
 setup(
     name="digital-land-data-manager",
     version=get_version(),
-    author="MHCLG Digital Land Team",
+    author="DLUHC Digital Land Team",
     author_email="DigitalLand@communities.gov.uk",
     license="MIT",
-    url="https://github.com/digital-land/pipeline",
+    url="https://github.com/digital-land/data-manager",
     packages=find_packages(exclude="tests"),
     install_requires=open("./requirements/requirements.txt").readlines(),
     dev_requires=open("./requirements/dev-requirements.txt").readlines(),
