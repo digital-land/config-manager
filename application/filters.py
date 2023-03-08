8dfe109defb613835cef03193f7ce7bd04ba5fa5
Paul Smith <mail@paulsmith.site>
2023-03-08 09:31:26 +0000

    adds debug filter

--- application/filters.py
+++ application/filters.py
@@ -1,5 +1,14 @@
+import json
+
+import jsonpickle
+
+
 def datasets_string_filter(datasets):
     s = ""
     for d in datasets:
         s = s + d.name + ";"
     return s
+
+
+def debug(thing):
+    return f"<script>console.log({json.dumps(json.loads(jsonpickle.encode(thing)), indent=2)});</script>"
