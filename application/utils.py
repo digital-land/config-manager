10db728d94f0e557e970db779855acd6a5145b69
Adam Shimali <adam.shimali@gmail.com>
2023-03-24 15:27:40 +0000

    remove unused code

--- application/utils.py
+++ application/utils.py
@@ -1,6 +1,4 @@
-import csv
 import hashlib
-import io
 from functools import wraps
 
 import requests
@@ -23,22 +21,6 @@ def check_url_reachable(url):
     except HTTPError as e:
         print(e)
         return False
-
-
-def csv_data_to_buffer(csv_rows):
-    out = io.StringIO()
-    fieldnames = csv_rows[0].keys()
-    writer = csv.DictWriter(
-        out, fieldnames=fieldnames, quoting=csv.QUOTE_MINIMAL, lineterminator="\r\n"
-    )
-    writer.writeheader()
-    for row in csv_rows:
-        writer.writerow(row)
-    buffer = io.BytesIO()
-    buffer.write(out.getvalue().encode())
-    buffer.seek(0)
-    out.close()
-    return buffer
 
 
 def login_required(f):
