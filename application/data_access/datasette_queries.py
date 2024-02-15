04c43265a437371de030679434d704d14972589b
CarlosCoelhoSL <ccoelho@scottlogic.com>
2024-02-15 15:33:18 +0000

    Adds error % graph Fixes dates on graph

--- application/data_access/datasette_queries.py
+++ application/data_access/datasette_queries.py
@@ -37,6 +37,30 @@ def get_datasette_query(db, sql, url="https://datasette.planning.data.gov.uk"):
         return None
 
 
+def get_logs():
+    year = datetime.datetime.today().year
+    sql = f"""
+        select year, month, week, day, status, count
+        from (
+            select
+                status,
+                strftime('%Y',entry_date) as year,
+                strftime('%m',entry_date) as month,
+                strftime('%W',entry_date) as week,
+                strftime('%d',entry_date) as day,
+                count(endpoint) as count
+            from log
+            group by year, week, status
+        ) as t1
+        where year = '{year}' or year = '{year-1}'
+    """
+    logs_df = get_datasette_query("digital-land", sql)
+    if logs_df is not None:
+        return logs_df
+    else:
+        return None
+
+
 def get_number_of_contributions():
     current_date = datetime.datetime.now().date()
     date_query = f" where substr(l.entry_date, 1, 10) = '{current_date}'"
@@ -69,50 +93,6 @@ def get_number_of_erroring_endpoints():
         return None
 
 
-def get_resources_downloaded_per_week():
-    year = datetime.datetime.today().year
-    sql = f"""
-        select year, month, week, day, count
-        from (
-            select
-                status,
-                strftime('%Y',entry_date) as year,
-                strftime('%m',entry_date) as month,
-                strftime('%W',entry_date) as week,
-                strftime('%d',entry_date) as day,
-                count(endpoint) as count
-            from log
-                where status = '200'
-            group by year, week
-        ) as t1
-        where year = '{year}' or year = '{year-1}'
-    """
-    resources_downloaded_df = get_datasette_query("digital-land", sql)
-    if resources_downloaded_df is not None:
-        current_year_resources_downloaded_df = resources_downloaded_df[
-            resources_downloaded_df["year"].astype(int) == year
-        ]
-        last_year_resources_downloaded_df = resources_downloaded_df[
-            resources_downloaded_df["year"].astype(int) == year - 1
-        ]
-        dates = []
-        counts = []
-        for df in [
-            current_year_resources_downloaded_df,
-            last_year_resources_downloaded_df,
-        ]:
-            year = int(df["year"].iloc[0])
-            week_numbers = resources_downloaded_df["week"].tolist()
-            for week in week_numbers:
-                day = 1 + (int(week) - 1) * 7
-                date = datetime.datetime(year, 1, 1) + datetime.timedelta(day - 1)
-                dates.append(date.strftime("%d/%m/%Y"))
-            counts.extend(df["count"].tolist())
-        return {"dates": dates[-20:], "counts": counts[-20:]}
-    else:
-        return None
-
-
 def get_endpoints_added_by_week():
     sql = """
         select
@@ -133,31 +113,116 @@ def get_endpoints_added_by_week():
         last_year_endpoints_added_df = endpoints_added_df[
             endpoints_added_df["year"].astype(int) == year - 1
         ]
-        dates = []
-        counts = []
-        for df in [last_year_endpoints_added_df, current_year_endpoints_added_df]:
-            year = int(df["year"].iloc[0])
-            week_numbers = df["week"].tolist()
-            for week in week_numbers:
-                day = 1 + (int(week) - 1) * 7
-                date = datetime.datetime(year, 1, 1) + datetime.timedelta(day - 1)
-                dates.append(date.strftime("%d/%m/%Y"))
-            counts.extend(df["count(endpoint)"].tolist())
-        return {"dates": dates[-20:], "counts": counts[-20:]}
+        # Cast week numbers to int now to prevent repeatedly converting in loop
+        current_year_endpoints_added_df["week"] = pd.to_numeric(
+            current_year_endpoints_added_df["week"]
+        )
+        last_year_endpoints_added_df["week"] = pd.to_numeric(
+            last_year_endpoints_added_df["week"]
+        )
+        dates = generate_dates(20)
+
+        endpoints_added = []
+        for date in dates:
+            year = date["year_number"]
+            week = date["week_number"]
+            for df in [last_year_endpoints_added_df, current_year_endpoints_added_df]:
+                df_year = int(df["year"].iloc[0])
+                if year == df_year:
+                    df_week_numbers = df["week"].tolist()
+                    if week in df_week_numbers:
+                        count = df.loc[df["week"] == week, "count(endpoint)"].values[0]
+                    else:
+                        count = 0
+                    endpoints_added.append(
+                        {"date": date["date"].strftime("%d/%m/%Y"), "count": count}
+                    )
+
+        return endpoints_added
     else:
         return None
 
 
+def get_endpoint_errors_and_successes_by_week(logs_df):
+    year = datetime.datetime.today().year
+    current_year_endpoint_successes_df = logs_df[
+        (logs_df["year"].astype(int) == year) & (logs_df["status"] == "200")
+    ]
+    last_year_endpoint_successes_df = logs_df[
+        (logs_df["year"].astype(int) == year - 1) & (logs_df["status"] == "200")
+    ]
+    current_year_endpoint_errors_df = logs_df[
+        (logs_df["year"].astype(int) == year) & (logs_df["status"] != "200")
+    ]
+    last_year_endpoint_errors_df = logs_df[
+        (logs_df["year"].astype(int) == year - 1) & (logs_df["status"] != "200")
+    ]
+
+    current_year_endpoint_successes_df["week"] = pd.to_numeric(
+        current_year_endpoint_successes_df["week"]
+    )
+    last_year_endpoint_successes_df["week"] = pd.to_numeric(
+        last_year_endpoint_successes_df["week"]
+    )
+    current_year_endpoint_errors_df["week"] = pd.to_numeric(
+        current_year_endpoint_errors_df["week"]
+    )
+    last_year_endpoint_errors_df["week"] = pd.to_numeric(
+        last_year_endpoint_errors_df["week"]
+    )
+
+    dates = generate_dates(20)
+
+    successes_by_week = []
+    error_percentages_by_week = []
+    for date in dates:
+        year = date["year_number"]
+        week = date["week_number"]
+        for df in [last_year_endpoint_successes_df, current_year_endpoint_successes_df]:
+            df_year = int(df["year"].iloc[0])
+            if year == df_year:
+                df_week_numbers = df["week"].tolist()
+                if week in df_week_numbers:
+                    successes = sum(df[(df["week"] == week)]["count"].tolist())
+                else:
+                    successes = 0
+                successes_by_week.append(
+                    {"date": date["date"].strftime("%d/%m/%Y"), "count": successes}
+                )
+
+        for df in [last_year_endpoint_errors_df, current_year_endpoint_errors_df]:
+            df_year = int(df["year"].iloc[0])
+            if year == df_year:
+                df_week_numbers = df["week"].tolist()
+                if week in df_week_numbers:
+                    errors = sum(df[(df["week"] == week)]["count"].tolist())
+                    error_percentage = 100 * errors / (errors + successes)
+                else:
+                    error_percentage = 0
+                error_percentages_by_week.append(
+                    {
+                        "date": date["date"].strftime("%d/%m/%Y"),
+                        "count": error_percentage,
+                    }
+                )
+
+    return successes_by_week, error_percentages_by_week
+
+
 def get_overview():
+    logs_df = get_logs()
     contributions = get_number_of_contributions()
     errors = get_number_of_erroring_endpoints()
     endpoints_added = get_endpoints_added_by_week()
-    resources_downloaded = get_resources_downloaded_per_week()
+    endpoint_successes, endpoint_errors = get_endpoint_errors_and_successes_by_week(
+        logs_df
+    )
     return {
         "contributions": contributions,
         "errors": errors,
         "endpoints_added": endpoints_added,
-        "resources_downloaded": resources_downloaded,
+        "resources_downloaded": endpoint_successes,
+        "error_percentages": endpoint_errors,
     }
 
 
@@ -193,3 +258,18 @@ def get_overview():
 #             missing.append(d["pipeline"])
 
 #     return all_datasets
+
+
+def generate_dates(number_of_weeks):
+    now = datetime.datetime.now()
+    monday = now - datetime.timedelta(days=now.weekday())
+    dates = []
+    for week in range(0, number_of_weeks):
+        date = monday - datetime.timedelta(weeks=week)
+        week_number = int(date.strftime("%W"))
+        year_number = int(date.year)
+        dates.append(
+            {"date": date, "week_number": week_number, "year_number": year_number}
+        )
+
+    return list(reversed(dates))
