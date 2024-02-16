1050218cb892938f537119256ff4d39ab7e48174
CarlosCoelhoSL <110818364+CarlosCoelhoSL@users.noreply.github.com>
2024-02-16 09:16:27 +0000

    Add reporting overview page to config manager (#9)
    * add blueprint for reporting
    
    * Adds contributions query
    
    * Adds erroring endpoints
    
    * endpoints added per week
    
    * Resources downloaded per week
    
    * adds node version to package
    
    * adds nonce to allow inline script
    
    * adds more nonces (test)
    
    * overview page formatting
    
    * Adds error % graph
    Fixes dates on graph
    
    * Restructures overview view
    
    * Fixes incorrect variable name in overview html
    
    * Tidies (some) commented out code
    
    * Tidies more commented out code
    
    ---------
    
    Co-authored-by: eveleighoj <35256612+eveleighoj@users.noreply.github.com>

---
+++ application/blueprints/report/views.py
@@ -0,0 +1,155 @@
+from flask import Blueprint, render_template
+
+from application.data_access.datasette_queries import (
+    get_endpoint_errors_and_successes_by_week,
+    get_endpoints_added_by_week,
+    get_logs,
+    get_number_of_contributions,
+    get_number_of_erroring_endpoints,
+)
+
+report_bp = Blueprint("reporting", __name__, url_prefix="/reporting")
+
+
+@report_bp.get("/")
+@report_bp.get("/overview")
+def overview():
+    # get tabbed elements
+    # odp_endpoint_summary = reporting_summary.groupby('dataset')  .....
+    # odp_active_resource_summary = ...
+    # dataset_summary = ....
+    # organisation_summary =  ...
+
+    # content_type_counts = sorted(
+    #     get_content_type_counts(),
+    #     key=lambda x: x["resource_count"],
+    #     reverse=True,
+    # )
+    logs_df = get_logs()
+    summary_contributions = get_number_of_contributions()
+    summary_endpoint_errors = get_number_of_erroring_endpoints()
+    endpoints_added_timeseries = get_endpoints_added_by_week()
+    (
+        endpoint_successes_timeseries,
+        endpoint_errors_timeseries,
+    ) = get_endpoint_errors_and_successes_by_week(logs_df)
+
+    summary_metrics = {
+        "contributions": summary_contributions,
+        "endpoint_errors": summary_endpoint_errors,
+    }
+    graphs = {
+        "endpoints_added_timeseries": endpoints_added_timeseries,
+        "endpoint_successes_timeseries": endpoint_successes_timeseries,
+        "endpoint_errors_timeseries": endpoint_errors_timeseries,
+    }
+
+    return render_template(
+        "reporting/overview.html", summary_metrics=summary_metrics, graphs=graphs
+    )
+
+
+# @report_bp.get("/dataset")
+# def datasets(dataset_id):
+#     dataset = Dataset.query.get(dataset_id)
+
+#     if dataset is None or dataset.collection_id is None:
+#         return abort(404)
+
+#     specification_pipelines = get_expected_pipeline_specs()
+
+#     rule_counts = count_pipeline_rules(dataset.collection.pipeline)
+
+#     return render_template(
+#         "dataset/dataset.html",
+#         pipeline=dataset.collection.pipeline,
+#         dataset=dataset,
+#         specification_pipelines=specification_pipelines,
+#         rule_counts=rule_counts,
+#     )
+
+
+# @report_bp.get("dataset/<string:dataset_id>")
+# def dataset(dataset_id, rule_type_name):
+#     limited = False
+#     dataset = Dataset.query.get(dataset_id)
+
+#     if dataset is None or dataset.collection_id is None:
+#         return abort(404)
+
+#     # # check if name is one of allowable rule types
+#     specification_pipelines = get_expected_pipeline_specs()
+#     if rule_type_name not in specification_pipelines.keys():
+#         return abort(404)
+
+#     rules = getattr(dataset.collection.pipeline, rule_type_name)
+
+#     if len(rules) > 1000:
+#         rules = rules[:1000]
+#         limited = True
+
+#     return render_template(
+#         "dataset/rules.html",
+#         dataset=dataset,
+#         rule_type_name=rule_type_name,
+#         rule_type_specification=specification_pipelines[rule_type_name],
+#         rules=rules,
+#         limited=limited,
+#     )
+
+
+# @report_bp.get("/organisation")
+# def organisations(dataset_id):
+#     dataset = Dataset.query.get(dataset_id)
+
+#     if dataset is None or dataset.collection_id is None:
+#         return abort(404)
+
+#     return render_template(
+#         "dataset/sources.html",
+#         dataset=dataset,
+#     )
+
+
+# @report_bp.get("/organisation/<string:organisation_id>")
+# def organisation(dataset_id, rule_type_name, rule_id):
+#     dataset = Dataset.query.get(dataset_id)
+
+#     if dataset is None or dataset.collection_id is None:
+#         return abort(404)
+
+#     specification_pipelines = get_expected_pipeline_specs()
+#     if rule_type_name not in specification_pipelines.keys():
+#         return abort(404)
+
+#     if rule_id == "new":
+#         # create empty rule except for dataset
+
+#         form = EditRuleForm(dataset_id=dataset.dataset)
+#         form.field_id.choices = [(field.field, field.field) for field in dataset.fields]
+#         rule = {"dataset": dataset.dataset}
+#     else:
+#         rule = get_rule(rule_id, rule_type_name)
+#         if rule is None:
+#             return abort(404)
+
+#         form = EditRuleForm(obj=rule, rule_type=rule_type_name)
+#         if hasattr(form, "field_id"):
+#             form.field_id.choices = [
+#                 (field.field, field.field) for field in dataset.fields
+#             ]
+#             if rule.field:
+#                 form.field_id.data = rule.field.field
+
+#     rule_type_specification = specification_pipelines[rule_type_name]
+#     form_field_names = _get_form_field_names(rule_type_specification)
+
+#     return render_template(
+#         "dataset/editrule.html",
+#         form=form,
+#         dataset=dataset,
+#         rule_type_name=rule_type_name,
+#         rule_type_specification=rule_type_specification,
+#         form_field_names=form_field_names,
+#         rule=rule,
+#     )
---
+++ application/data_access/datasette_queries.py
@@ -0,0 +1,256 @@
+import datetime
+import logging
+
+import pandas as pd
+import requests
+from requests import adapters
+from urllib3 import Retry
+
+
+def get_datasette_http():
+    """
+    Function to return  http for the use of querying  datasette,
+    specifically to add retries for larger queries
+    """
+    retry_strategy = Retry(total=3, status_forcelist=[400], backoff_factor=0)
+
+    adapter = adapters.HTTPAdapter(max_retries=retry_strategy)
+
+    http = requests.Session()
+    http.mount("https://", adapter)
+    http.mount("http://", adapter)
+
+    return http
+
+
+def get_datasette_query(db, sql, url="https://datasette.planning.data.gov.uk"):
+    url = f"{url}/{db}.json"
+    params = {"sql": sql, "_shape": "array"}
+    try:
+        http = get_datasette_http()
+        resp = http.get(url, params=params)
+        resp.raise_for_status()
+        df = pd.DataFrame.from_dict(resp.json())
+        return df
+    except Exception as e:
+        logging.warning(e)
+        return None
+
+
+def get_logs():
+    year = datetime.datetime.today().year
+    sql = f"""
+        select year, month, week, day, status, count
+        from (
+            select
+                status,
+                strftime('%Y',entry_date) as year,
+                strftime('%m',entry_date) as month,
+                strftime('%W',entry_date) as week,
+                strftime('%d',entry_date) as day,
+                count(endpoint) as count
+            from log
+            group by year, week, status
+        ) as t1
+        where year = '{year}' or year = '{year-1}'
+    """
+    logs_df = get_datasette_query("digital-land", sql)
+    if logs_df is not None:
+        return logs_df
+    else:
+        return None
+
+
+def get_number_of_contributions():
+    current_date = datetime.datetime.now().date()
+    date_query = f" where substr(l.entry_date, 1, 10) = '{current_date}'"
+    sql = f"""
+        select count(*) as count
+            from log l
+            {date_query}
+            and l.status = 200
+    """
+    contributions_df = get_datasette_query("digital-land", sql)
+    if contributions_df is not None:
+        return int(contributions_df.iloc[0]["count"])
+    else:
+        return None
+
+
+def get_number_of_erroring_endpoints():
+    current_date = datetime.datetime.now().date()
+    date_query = f" where substr(l.entry_date, 1, 10) = '{current_date}'"
+    sql = f"""
+        select count(*) as count
+            from log l
+            {date_query}
+            and l.status != 200
+    """
+    errors_df = get_datasette_query("digital-land", sql)
+    if errors_df is not None:
+        return int(errors_df.iloc[0]["count"])
+    else:
+        return None
+
+
+def get_endpoints_added_by_week():
+    sql = """
+        select
+            strftime('%Y',entry_date) as year,
+            strftime('%m',entry_date) as month,
+            strftime('%W',entry_date) as week,
+            strftime('%d',entry_date) as day,
+            count(endpoint)
+        from endpoint
+        group by year, week
+    """
+    endpoints_added_df = get_datasette_query("digital-land", sql)
+    year = datetime.datetime.today().year
+    if endpoints_added_df is not None:
+        current_year_endpoints_added_df = endpoints_added_df[
+            endpoints_added_df["year"].astype(int) == year
+        ]
+        last_year_endpoints_added_df = endpoints_added_df[
+            endpoints_added_df["year"].astype(int) == year - 1
+        ]
+        # Cast week numbers to int now to prevent repeatedly converting in loop
+        current_year_endpoints_added_df["week"] = pd.to_numeric(
+            current_year_endpoints_added_df["week"]
+        )
+        last_year_endpoints_added_df["week"] = pd.to_numeric(
+            last_year_endpoints_added_df["week"]
+        )
+        dates = generate_dates(20)
+
+        endpoints_added = []
+        for date in dates:
+            year = date["year_number"]
+            week = date["week_number"]
+            for df in [last_year_endpoints_added_df, current_year_endpoints_added_df]:
+                df_year = int(df["year"].iloc[0])
+                if year == df_year:
+                    df_week_numbers = df["week"].tolist()
+                    if week in df_week_numbers:
+                        count = df.loc[df["week"] == week, "count(endpoint)"].values[0]
+                    else:
+                        count = 0
+                    endpoints_added.append(
+                        {"date": date["date"].strftime("%d/%m/%Y"), "count": count}
+                    )
+
+        return endpoints_added
+    else:
+        return None
+
+
+def get_endpoint_errors_and_successes_by_week(logs_df):
+    year = datetime.datetime.today().year
+    current_year_endpoint_successes_df = logs_df[
+        (logs_df["year"].astype(int) == year) & (logs_df["status"] == "200")
+    ]
+    last_year_endpoint_successes_df = logs_df[
+        (logs_df["year"].astype(int) == year - 1) & (logs_df["status"] == "200")
+    ]
+    current_year_endpoint_errors_df = logs_df[
+        (logs_df["year"].astype(int) == year) & (logs_df["status"] != "200")
+    ]
+    last_year_endpoint_errors_df = logs_df[
+        (logs_df["year"].astype(int) == year - 1) & (logs_df["status"] != "200")
+    ]
+
+    current_year_endpoint_successes_df["week"] = pd.to_numeric(
+        current_year_endpoint_successes_df["week"]
+    )
+    last_year_endpoint_successes_df["week"] = pd.to_numeric(
+        last_year_endpoint_successes_df["week"]
+    )
+    current_year_endpoint_errors_df["week"] = pd.to_numeric(
+        current_year_endpoint_errors_df["week"]
+    )
+    last_year_endpoint_errors_df["week"] = pd.to_numeric(
+        last_year_endpoint_errors_df["week"]
+    )
+
+    dates = generate_dates(20)
+
+    successes_by_week = []
+    error_percentages_by_week = []
+    for date in dates:
+        year = date["year_number"]
+        week = date["week_number"]
+        for df in [last_year_endpoint_successes_df, current_year_endpoint_successes_df]:
+            df_year = int(df["year"].iloc[0])
+            if year == df_year:
+                df_week_numbers = df["week"].tolist()
+                if week in df_week_numbers:
+                    successes = sum(df[(df["week"] == week)]["count"].tolist())
+                else:
+                    successes = 0
+                successes_by_week.append(
+                    {"date": date["date"].strftime("%d/%m/%Y"), "count": successes}
+                )
+
+        for df in [last_year_endpoint_errors_df, current_year_endpoint_errors_df]:
+            df_year = int(df["year"].iloc[0])
+            if year == df_year:
+                df_week_numbers = df["week"].tolist()
+                if week in df_week_numbers:
+                    errors = sum(df[(df["week"] == week)]["count"].tolist())
+                    error_percentage = 100 * errors / (errors + successes)
+                else:
+                    error_percentage = 0
+                error_percentages_by_week.append(
+                    {
+                        "date": date["date"].strftime("%d/%m/%Y"),
+                        "count": error_percentage,
+                    }
+                )
+
+    return successes_by_week, error_percentages_by_week
+
+
+# def get_datasets_summary():
+#     # get all the datasets listed with their active status
+#     all_datasets = index_by("dataset", get_datasets())
+#     missing = []
+
+#     # add the publisher coverage numbers
+#     dataset_coverage = publisher_coverage()
+#     for d in dataset_coverage:
+#         if all_datasets.get(d["pipeline"]):
+#             all_datasets[d["pipeline"]] = {**all_datasets[d["pipeline"]], **d}
+#         else:
+#             missing.append(d["pipeline"])
+
+#     # add the total resource count
+#     dataset_resource_counts = resources_by_dataset()
+#     for d in dataset_resource_counts:
+#         if all_datasets.get(d["pipeline"]):
+#             all_datasets[d["pipeline"]] = {**all_datasets[d["pipeline"]], **d}
+#         else:
+#             missing.append(d["pipeline"])
+
+#     # add the first and last resource dates
+#     dataset_resource_dates = first_and_last_resource()
+#     for d in dataset_resource_dates:
+#         if all_datasets.get(d["pipeline"]):
+#             all_datasets[d["pipeline"]] = {**all_datasets[d["pipeline"]], **d}
+#         else:
+#             missing.append(d["pipeline"])
+
+#     return all_datasets
+
+
+def generate_dates(number_of_weeks):
+    now = datetime.datetime.now()
+    monday = now - datetime.timedelta(days=now.weekday())
+    dates = []
+    for week in range(0, number_of_weeks):
+        date = monday - datetime.timedelta(weeks=week)
+        week_number = int(date.strftime("%W"))
+        year_number = int(date.year)
+        dates.append(
+            {"date": date, "week_number": week_number, "year_number": year_number}
+        )
+
+    return list(reversed(dates))
