274c45e4e191fe0873385c51e3fbacb286570396
CarlosCoelhoSL <ccoelho@scottlogic.com>
2024-04-18 15:05:02 +0100

    Adds comments

--- application/data_access/odp_queries.py
+++ application/data_access/odp_queries.py
@@ -532,6 +532,7 @@ def create_issue_row(organisation, cohort, name, issue_df, datasets):
 
 
 def get_odp_issues_by_issue_type(dataset_types, cohorts):
+    # Separate method for issue download as granularity required is different from the issue summary
     filtered_cohorts = [
         x for x in cohorts if cohorts[0] in [cohort["id"] for cohort in COHORTS]
     ]
@@ -640,10 +641,12 @@ def get_odp_issues_by_issue_type(dataset_types, cohorts):
 
 
 def generate_odp_summary_csv(odp_summary):
+    # Can convert any Dataframe to csv
     if type(odp_summary) is pd.DataFrame:
         with tempfile.NamedTemporaryFile(mode="w", delete=False) as csvfile:
             odp_summary.to_csv(csvfile, index=False)
             return csvfile.name
+    # Else assume odp summary structure
     else:
         dfs = []
         for row in odp_summary["rows"]:
