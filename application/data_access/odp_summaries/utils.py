339b6ad18a0b391e259b6fd4d9026ee7051d89cc
CarlosCoelhoSL <ccoelho@scottlogic.com>
2024-08-16 16:10:51 +0100

    refactors issue page to use performance db

--- application/data_access/odp_summaries/utils.py
+++ application/data_access/odp_summaries/utils.py
@@ -2,6 +2,8 @@ import tempfile
 
 import pandas as pd
 
+from application.data_access.datasette_utils import get_datasette_query
+
 
 def generate_odp_summary_csv(odp_summary):
     # Can convert any Dataframe to csv
@@ -23,3 +25,39 @@ def generate_odp_summary_csv(odp_summary):
         with tempfile.NamedTemporaryFile(mode="w", delete=False) as csvfile:
             output_df.to_csv(csvfile, index=False)
             return csvfile.name
+
+
+def get_provisions(selected_cohorts, all_cohorts):
+    filtered_cohorts = [
+        x
+        for x in selected_cohorts
+        if selected_cohorts[0] in [cohort["id"] for cohort in all_cohorts]
+    ]
+    cohort_clause = (
+        "AND " + " or ".join(("c.cohort = '" + str(n) + "'" for n in filtered_cohorts))
+        if filtered_cohorts
+        else ""
+    )
+    sql = f"""
+    SELECT
+        p.cohort,
+        p.organisation,
+        p.project,
+        p.provision_reason,
+        c.start_date as cohort_start_date
+    FROM
+        provision p
+    INNER JOIN
+        cohort c on c.cohort = p.cohort
+    WHERE
+        p.provision_reason = "expected"
+    AND p.project == "open-digital-planning"
+    {cohort_clause}
+    GROUP BY
+        p.organisation
+    ORDER BY
+        cohort_start_date,
+        p.cohort
+    """
+    provision_df = get_datasette_query("digital-land", sql)
+    return provision_df
