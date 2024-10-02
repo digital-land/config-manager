f25935faa98a19df8cb59ecb46a11255e96cbc16
CarlosCoelhoSL <ccoelho@scottlogic.com>
2024-10-02 09:08:33 +0100

    accounts for perf db general changes

--- application/data_access/odp_summaries/conformance.py
+++ application/data_access/odp_summaries/conformance.py
@@ -225,9 +225,7 @@ def get_odp_conformance_summary(dataset_types, cohorts):
                 "endpoint",
                 "endpoint_no.",
                 "resource",
-                "status",
                 "latest_log_entry_date",
-                "endpoint_entry_date",
                 "cohort_start_date",
             ]
         )
@@ -300,9 +298,7 @@ def get_odp_conformance_summary(dataset_types, cohorts):
         "endpoint",
         "endpoint_no.",
         "resource",
-        "status",
         "latest_log_entry_date",
-        "endpoint_entry_date",
         "field",
         "field_supplied",
         "field_matched",
