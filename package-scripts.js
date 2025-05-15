20348c276bb01166e015ccd4704416732b309fa0
Ben Hodgkiss <ben.hodgkiss@levellingup.gov.uk>
2025-05-15 17:13:15 +0100

    Update package-scripts.js

--- package-scripts.js
+++ package-scripts.js
@@ -5,7 +5,7 @@ module.exports = {
     copy: "npm-run-all --parallel copy.*",
 
     "build.stylesheets": "sass src/scss:application/static/stylesheets " +
-      "--load-path=node_modules/govuk-frontend/dist " +
+      "--load-path=node_modules/govuk-frontend " +
       "--load-path=node_modules/digital-land-frontend " +
       "--load-path=src/scss/components " +
       "--load-path=src/scss/common",
