b75cb14b094a6f3b3940604d104566b0806bcb79
Ben Hodgkiss <ben.hodgkiss@levellingup.gov.uk>
2025-05-15 17:05:31 +0100

    Update package-scripts.js

--- package-scripts.js
+++ package-scripts.js
@@ -1,16 +1,15 @@
 module.exports = {
   scripts: {
-    copy: {
-      css: "rsync -ar src/css/vendor/ application/static/stylesheets/vendor",
-      js: "rsync -ar src/javascripts/vendor/ application/static/javascripts/vendor"
-    },
-    build: {
-      stylesheets: "sass src/scss:application/static/stylesheets " +
-        "--load-path=node_modules/govuk-frontend/dist " +
-        "--load-path=node_modules/digital-land-frontend " +
-        "--load-path=src/scss/components " +
-        "--load-path=src/scss/common",
-      javascripts: "esbuild src/javascripts/main.js --bundle --outfile=application/static/javascripts/bundle.js"
-    }
+    "copy.javascripts": "rsync -ar src/javascripts/vendor/ application/static/javascripts/vendor",
+    "copy.css": "rsync -ar src/css/vendor/ application/static/stylesheets/vendor",
+    copy: "npm-run-all --parallel copy.*",
+
+    "build.stylesheets": "sass src/scss:application/static/stylesheets " +
+      "--load-path=node_modules/govuk-frontend/dist " +
+      "--load-path=node_modules/digital-land-frontend " +
+      "--load-path=src/scss/components " +
+      "--load-path=src/scss/common",
+
+    "build.javascripts": "esbuild src/javascripts/main.js --bundle --outfile=application/static/javascripts/bundle.js"
   }
 };
