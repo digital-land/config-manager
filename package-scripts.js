814ebdaaa6be574be40816eefdf8e2c33132708d
Ben Hodgkiss <ben.hodgkiss@levellingup.gov.uk>
2025-05-15 16:58:52 +0100

    Update package-scripts.js

--- package-scripts.js
+++ package-scripts.js
@@ -1,11 +1,16 @@
 module.exports = {
   scripts: {
+    copy: {
+      css: "rsync -ar src/css/vendor/ application/static/stylesheets/vendor",
+      js: "rsync -ar src/javascripts/vendor/ application/static/javascripts/vendor"
+    },
     build: {
       stylesheets: "sass src/scss:application/static/stylesheets " +
         "--load-path=node_modules/govuk-frontend/dist " +
         "--load-path=node_modules/digital-land-frontend " +
         "--load-path=src/scss/components " +
-        "--load-path=src/scss/common"
+        "--load-path=src/scss/common",
+      javascripts: "esbuild src/javascripts/main.js --bundle --outfile=application/static/javascripts/bundle.js"
     }
   }
 };
