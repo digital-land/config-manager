36d442863ea9998460753d5725eec8abc8a3ac79
“Kavitha.Tulluru” <kavitha.tulluru@communities.gov.uk>
2025-07-17 13:35:14 +0100

    DataManager UI service: DataManager Landing Page (GitAuth already included) integration to existing config-manager repo#68

--- application/blueprints/datamanager/views.py
+++ application/blueprints/datamanager/views.py
@@ -6,9 +6,26 @@ from flask import Blueprint, abort, redirect, render_template, request, url_for
 datamanager_bp = Blueprint("datamanager", __name__, url_prefix="/datamanager")
 
 
-@datamanager_bp.get("/")
+@datamanager_bp.route("/")
 def index():
+    datamanager = {"name": "Dashboard"}  
+    return render_template("datamanager/index.html", datamanager=datamanager)
+
+
+@datamanager_bp.route("/dashboard/add")
+def dashboard_add():
     """
-    Render the main datamanager page.
+    Render the dashboard add page.
     """
-    return render_template("datamanager/index.html")
+
+    return render_template("datamanager/dashboard_add.html")
+
+
+@datamanager_bp.route("/dashboard/config")
+def dashboard_config():
+    """
+    Render the dashboard configuration page.
+    """
+    return render_template("datamanager/dashboard_config.html")
+
+
